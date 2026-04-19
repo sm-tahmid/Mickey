a69bd7ed11019d75fd416f0ce93fc25d83710f33
S M. Tahmid Abir <153380490+sm-tahmid@users.noreply.github.com>
2026-04-19 15:46:22 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'mickey-tracker-v1';
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request)
+          .then(fetchRes => {
+            return caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, fetchRes.clone());
+              return fetchRes;
+            });
+          })
+          .catch(() => caches.match('./index.html'));
+      })
+  );
+});
